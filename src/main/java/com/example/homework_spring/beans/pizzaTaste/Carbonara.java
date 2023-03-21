6c3c443a33c23fb1321546e6e553264bd757c2e5
DariInel <adara21210@gmail.com>
2023-03-21 20:32:52 +0500

    New project

--- src/main/java/com/example/homework_spring/beans/pizzaTaste/Carbonara.java
+++
@@ -1,7 +0,0 @@
-package com.example.homework_spring.beans.pizzaTaste;
-
-public class Carbonara extends PizzaTaste {
-    public Carbonara() {
-        super(300);
-    }
-}
--- src/main/java/com/example/homework_spring/beans/pizzaTaste/Gorgonzola.java
+++
@@ -1,7 +0,0 @@
-package com.example.homework_spring.beans.pizzaTaste;
-
-public class Gorgonzola extends PizzaTaste{
-    public Gorgonzola() {
-        super(400);
-    }
-}
